5556c1b2d7f45c3e6ea6f8c9d6fe5659d43177ad
news structure <maisonatelier52@gmail.com>
2026-03-02 16:07:15 +0530

    solving the heading order issue on category page

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,35 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//  images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'i.pravatar.cc',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'encrypted-tbn0.gstatic.com',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-      },
-    ],
+  output:'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
+  // other config options can go here
 };
 
 export default nextConfig;
