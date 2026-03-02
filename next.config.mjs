fd4f876331af40caef6b93ab7b6a88ba3b09d10f
news structure <maisonatelier52@gmail.com>
2026-03-02 16:12:45 +0530

    chnage the updated code of next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,35 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//  images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'images.unsplash.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'i.pravatar.cc',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'encrypted-tbn0.gstatic.com',
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
+ images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
+    ],
   },
-  // other config options can go here
 };
 
 export default nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output:'export',
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true,
+//   },
+//   // other config options can go here
+// };
+
+// export default nextConfig;
